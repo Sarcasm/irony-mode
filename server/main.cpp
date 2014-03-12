ddcd4c1e63da42b3103fef16b2666423a72d8187
Guillaume Papin <guillaume.papin@epitech.eu>
2014-03-12 23:00:01 +0100

    irony-server: introduce version 0.1

--- server/main.cpp
+++ server/main.cpp
@@ -10,12 +10,40 @@
 #include "Server.h"
 
 #include <clocale>
+#include <cstdlib>
 #include <iomanip>
 #include <iostream>
+#include <vector>
 
-int main() {
+static void parseCommandLine(const std::vector<std::string> &argv) {
+  for (std::vector<std::string>::const_iterator it = argv.begin() + 1,
+                                                end = argv.end();
+       it != end;
+       ++it) {
+    const std::string &arg = *it;
+
+    if (arg == "--version") {
+      // do not change the format for the first line, external programs should
+      // be able to rely on it
+      std::cout << "irony-mode version 0.1" << std::endl;
+      exit(0);
+    }
+
+    if (arg == "--help" || arg == "-h") {
+      std::cout << "usage: " << argv[0] << " [-h] [--version]\n";
+      exit(0);
+    } else {
+      std::cerr << argv[0] << ": error: unrecognized argment '" << arg << "'\n";
+      exit(1);
+    }
+  }
+}
+
+int main(int argc, const char *argv[]) {
   // Required for wstring
   setlocale(LC_CTYPE, "");
+
+  parseCommandLine(std::vector<std::string>(argv, &argv[argc]));
 
   Server server;
 
