b1a762cfa4d390a6d6041248048bb36a03911bc4
Guillaume Papin <guillaume.papin@epitech.eu>
2014-07-21 23:53:34 +0200

    CMake: detect and use libc++ when needed
    See https://github.com/Sarcasm/irony-mode/issues/113

--- server/cmake/utils.cmake
+++ server/cmake/utils.cmake
@@ -1,4 +1,5 @@
 include(CheckCXXCompilerFlag)
+include(CheckCXXSourceCompiles)
 
 #
 # check_for_in_source_build()
@@ -75,47 +76,66 @@ function(enable_colored_diagnotics)
 endfunction()
 
 #
-# check_cxx11_compiler_option()
+# check_cxx11_options()
 #
-# Throws a FATAL_ERROR if C++11 isn't available otherwise sets
-# CXX11_COMPILER_OPTIONS with the proper compiler option to enable C++11 (if
-# any).
+# Throws a FATAL_ERROR if C++11 isn't available otherwise sets:
+# - CXX11_COMPILE_OPTIONS
+# - CXX11_LINK_OPTIONS
 #
-function(check_cxx11_compiler_options)
-  if (CXX11_COMPILER_OPTIONS)
+function(check_cxx11_options)
+  if (CXX11_COMPILE_OPTIONS)
     return() # already in cache
   endif()
 
   if (CMAKE_CXX_COMPILER_ID MATCHES "GNU|Clang")
     check_cxx_compiler_flag("-std=c++11" HAS_STDCXX11)
     if (HAS_STDCXX11)
-      set(cxx11_options -std=c++11)
+      set(compile_options -std=c++11)
     else()
       check_cxx_compiler_flag("-std=c++0x" HAS_STDCXX0X)
       if (HAS_STDCXX0X)
-        set(cxx11_options -std=c++0x)
+        set(compile_options -std=c++0x)
       else()
         message(FATAL_ERROR "Basic C++11 support is required!")
       endif()
     endif()
+
+    # Check whether or not the system library provides proper C++11 support, if
+    # not we try to link specifically against libc++.
+    #
+    # This seems useful for Mac OS X builds, see:
+    # http://cplusplusmusings.wordpress.com/2012/07/05/clang-and-standard-libraries-on-mac-os-x/
+    set(CMAKE_REQUIRED_FLAGS ${compile_options})
+    check_cxx_source_compiles("#include <random>
+
+int main() {
+    std::random_device rd;
+    std::default_random_engine e(rd());
+    std::uniform_int_distribution<int> dist(0, 15);
+
+    return dist(e);
+}
+"
+      HAS_CXX11_STDLIB)
+
+    if (NOT HAS_CXX11_STDLIB)
+      check_cxx_compiler_flag("-stdlib=libc++" HAS_LIBCXX)
+
+      if (HAS_LIBCXX)
+        list(APPEND compile_options -stdlib=libc++)
+        list(APPEND link_options -stdlib=libc++)
+      else()
+        message(FATAL_ERROR "Standard library doesn't support C++11!")
+      endif()
+    endif()
   endif()
 
-  if (cxx11_options)
-    message(STATUS "C++11 compiler option is ${cxx11_options}")
+  if (compile_options)
+    message(STATUS "C++11 compiler option(s): ${compile_options}")
   endif()
-  set(CXX11_COMPILER_OPTIONS ${cxx11_options} CACHE INTERNAL
+
+  set(CXX11_COMPILE_OPTIONS ${compile_options} CACHE INTERNAL
     "C++11 compile options, if any")
-endfunction()
-
-#
-# enable_cxx11_compile_options()
-#
-# Test for C++11 availability and setup the compiler options needed if any.
-# Otherwise throw an error.
-#
-function(enable_cxx11_compile_options)
-  check_cxx11_compiler_options()
-  if (CXX11_COMPILER_OPTIONS)
-    add_compile_options_(${CXX11_COMPILER_OPTIONS})
-  endif()
+  set(CXX11_LINK_OPTIONS ${link_options} CACHE INTERNAL
+    "C++11 link options, if any")
 endfunction()
