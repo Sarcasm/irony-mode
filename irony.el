7fd81b1b679b2cdc95588b1506c9bedb575ee1e5
Karl Hylén <karl.hylen@gmail.com>
2015-04-09 19:55:05 +0200

    Fixed some comments in irony.el.

--- irony.el
+++ irony.el
@@ -666,11 +666,8 @@ If no such file exists on the filesystem the special file '-' is
                                      (combine-and-quote-strings argv)))))))
 
 (defun irony--send-request-sync (request callback &rest args)
-  "Send a request to irony-server and wait for the result.
-
-callback should set the irony-sync-result property of the irony-server process"
+  "Send a request to irony-server and wait for the result."
   (let ((process (irony--get-server-process-create)))
-    ;; Using apply to "spread" the args list
     (apply 'irony--send-request request callback args)
     ;; While there are callbacks left on the queue, wait for output from server
     (while (irony--server-process-has-callback-p process)
