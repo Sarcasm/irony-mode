3d7fe7cd43a070a57527d7a8fd1b98bc086848b8
Guillaume Papin <guillaume.papin@parrot.com>
2015-08-31 10:44:57 +0200

    FindLibClang.cmake: add 3.8 version support
    http://llvm.org/apt/ provides 3.8 since LLVM 3.7 has been released.

--- server/cmake/modules/FindLibClang.cmake
+++ server/cmake/modules/FindLibClang.cmake
@@ -15,7 +15,8 @@
 
 # most recent versions come first
 # http://llvm.org/apt/
-set(LIBCLANG_KNOWN_LLVM_VERSIONS 3.7.0 3.7
+set(LIBCLANG_KNOWN_LLVM_VERSIONS 3.8.0 3.8
+  3.7.0 3.7
   3.6.2 3.6.1 3.6.0 3.6
   3.5.2 3.5.1 3.5.0 3.5
   3.4.2 3.4.1 3.4
