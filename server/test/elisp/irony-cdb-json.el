428ed331491781253c2f296119c28854f7444ef8
MikeSanMichel2 <MikeSanMichel2@users.noreply.github.com>
2015-10-15 02:22:28 +0300

    tests for choose-closest-path

--- server/test/elisp/irony-cdb-json.el
+++ server/test/elisp/irony-cdb-json.el
@@ -27,3 +27,33 @@
    (equal "/home/user/project/build"
           (nth 2 (irony-cdb-json--transform-compile-command
                   irony-cdb/compile-command)))))
+
+(ert-deftest cdb/choose-closest-path/chooses-closest ()
+  (should
+   (equal "/tmp/a/cdb"
+          (irony-cdb--choose-closest-path "/tmp/a/1"
+                                          '("/tmp/a/cdb" "/tmp/cdb")))))
+
+(ert-deftest cdb/choose-closest-path/chooses-closest2 ()
+  (should
+   (equal "/tmp/a/cdb"
+          (irony-cdb--choose-closest-path "/tmp/a/1"
+                                          '("/tmp/cdb" "/tmp/a/cdb")))))
+
+(ert-deftest cdb/choose-closest-path/prefers-deeper ()
+  (should
+   (equal "/tmp/a/build/cdb"
+          (irony-cdb--choose-closest-path "/tmp/a/1"
+                                          '("/tmp/a/build/cdb" "/tmp/cdb")))))
+
+(ert-deftest cdb/choose-closest-path/prefers-deeper2 ()
+  (should
+   (equal "/tmp/a/build/cdb"
+          (irony-cdb--choose-closest-path "/tmp/a/1"
+                                          '("/tmp/cdb" "/tmp/a/build/cdb")))))
+
+(ert-deftest cdb/choose-closest-path/will-survive-garbage ()
+  (should
+   (equal nil
+          (irony-cdb--choose-closest-path "/tmp/a/1"
+                                          'ordures))))
