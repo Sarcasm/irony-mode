6e0a3f0df9306bda32dfe6097ffa014a23c05af6
Guillaume Papin <guillaume.papin@epitech.eu>
2016-07-13 21:45:21 +0200

    FindLibClang.cmake: add support for 3.8.1 release

--- server/cmake/modules/FindLibClang.cmake
+++ server/cmake/modules/FindLibClang.cmake
@@ -16,7 +16,7 @@
 # most recent versions come first
 # http://llvm.org/apt/
 set(LIBCLANG_KNOWN_LLVM_VERSIONS 3.9.0 3.9
-  3.8.0 3.8
+  3.8.1 3.8.0 3.8
   3.7.1 3.7.0 3.7
   3.6.2 3.6.1 3.6.0 3.6
   3.5.2 3.5.1 3.5.0 3.5
