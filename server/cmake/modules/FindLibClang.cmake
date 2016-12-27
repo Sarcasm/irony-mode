ebc373b0b4f2b3f491c91d3a7f6dc9ce565a2960
Guillaume Papin <guillaume.papin@parrot.com>
2016-12-27 12:48:01 +0100

    FindLibClang.cmake: add support for 3.9.1

--- server/cmake/modules/FindLibClang.cmake
+++ server/cmake/modules/FindLibClang.cmake
@@ -16,7 +16,7 @@
 # most recent versions come first
 # http://llvm.org/apt/
 set(LIBCLANG_KNOWN_LLVM_VERSIONS 4.0.0 4.0
-  3.9.0 3.9
+  3.9.1 3.9.0 3.9
   3.8.1 3.8.0 3.8
   3.7.1 3.7.0 3.7
   3.6.2 3.6.1 3.6.0 3.6
