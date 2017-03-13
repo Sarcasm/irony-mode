4d64ecc0970ff43290b4b27e0c7d2176d1890c71
Guillaume Papin <guillaume.papin@epitech.eu>
2017-03-13 22:37:23 +0100

    FindLibClang.cmake: add support for 5.0
    LLVM 4.0 has been released today (announcement:
    http://lists.llvm.org/pipermail/llvm-announce/2017-March/000073.html).
    
    5.0 is (and has been for a while now) the trunk.

--- server/cmake/modules/FindLibClang.cmake
+++ server/cmake/modules/FindLibClang.cmake
@@ -15,7 +15,8 @@
 
 # most recent versions come first
 # http://llvm.org/apt/
-set(LIBCLANG_KNOWN_LLVM_VERSIONS 4.0.0 4.0
+set(LIBCLANG_KNOWN_LLVM_VERSIONS 5.0.0 5.0
+  4.0.0 4.0
   3.9.1 3.9.0 3.9
   3.8.1 3.8.0 3.8
   3.7.1 3.7.0 3.7
