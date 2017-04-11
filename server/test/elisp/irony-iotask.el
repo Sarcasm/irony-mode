daff67b36acb93c11badc25a4e0b7944e3fd2138
Guillaume Papin <guillaume.papin@epitech.eu>
2017-04-12 01:36:03 +0200

    irony-iotask: simplify tests

--- server/test/elisp/irony-iotask.el
+++ server/test/elisp/irony-iotask.el
@@ -12,6 +12,16 @@
     (add-to-list 'load-path irony-iotask-dir)
     (require 'irony-iotask)))
 
+(defun irony-iotask-echo-process-exit-filter (process output)
+  (when (buffer-live-p (process-buffer process))
+    (with-current-buffer (process-buffer process)
+      (goto-char (process-mark process))
+      (insert output)
+      (set-marker (process-mark process) (point))
+      (when (>= (buffer-size) (length "exit\n"))
+        (should (equal (buffer-substring-no-properties (point-min) (point-max))
+                       "exit\n"))))))
+
 ;; Note: these tests use process communication with the standard I/O streams.
 ;; The subprocess used for this communication is Emacs.
 ;;
@@ -20,8 +30,7 @@
 ;; streams works in Elisp scripts:
 ;; - http://www.lunaryorn.com/2014/08/12/emacs-script-pitfalls.html
 
-(defmacro irony-iotask/with-elisp-process-setup (process-script
-                                                 &rest body)
+(defmacro irony-iotask/with-echo-process (&rest body)
   "Start an Emacs process that runs the given PROCESS-SCRIPT.
 
 The process is setup with `irony-iotask-setup-process'.
@@ -45,24 +54,23 @@ available on all OSes irony-iotask support."
                           "-Q"
                           "--batch"
                           "--eval"
-                          (prin1-to-string (quote ,process-script))))
+                          (prin1-to-string
+                           '(let ((msg))
+                              (while (not (equal msg "exit"))
+                                (setq msg (read-from-minibuffer ""))
+                                (message msg))))))
      (unwind-protect
          (progn
            (irony-iotask-setup-process process)
            ,@body)
        ;; for the tests, we want to wait the end of the process
-       (unwind-protect
-           (with-timeout
-               (1
-                (kill-process process))
-             (while (process-live-p process)
-               (sit-for 0.05)))))))
-
-(defmacro irony-iotask/with-echo-process-setup (&rest body)
-  (declare (indent 1))
-  `(irony-iotask/with-elisp-process-setup
-    (message (read-from-minibuffer ""))
-    ,@body))
+       (with-current-buffer (process-buffer process)
+         (erase-buffer))
+       (set-process-filter process #'irony-iotask-echo-process-exit-filter)
+       (process-send-string process "exit\n")
+       (with-timeout (1 (kill-process process))
+         (while (process-live-p process)
+           (sit-for 0.05))))))
 
 ;; irony-iotask-result
 
@@ -115,15 +123,12 @@ available on all OSes irony-iotask support."
 
 (ert-deftest irony-iotask/task-start/simple ()
   (let ((task (irony-iotask-package-task irony-iotask/task-start-t)))
-    (irony-iotask/with-elisp-process-setup
-     (read-from-minibuffer "")
-     (should (equal 42 (irony-iotask-run process task)))
-     (process-send-string process "\n"))))
+    (irony-iotask/with-echo-process
+     (should (equal 42 (irony-iotask-run process task))))))
 
 (ert-deftest irony-iotask/task-start/with-arguments ()
   (let ((task (irony-iotask-package-task irony-iotask/task-start-t 43)))
-    (irony-iotask/with-elisp-process-setup
-     () ;; no-op
+    (irony-iotask/with-echo-process
      (should (equal 43 (irony-iotask-run process task))))))
 
 (irony-iotask-define-task irony-iotask/task-update-t
@@ -132,28 +137,29 @@ available on all OSes irony-iotask support."
            (irony-iotask-send-string (format "%s\n" (or hello "hello"))))
   :update (lambda (&optional hello)
             (setq hello (or hello "hello"))
-            (cond
-             ((string= (buffer-string) (format "%s\n" hello))
-              (irony-iotask-set-result (format "%s ok" hello)))
-             ((>= (buffer-size) (1+ (length hello)))
-              (throw 'invalid-msg t)))))
+            (when (string= (buffer-string) (format "%s\n" hello))
+              (irony-iotask-set-result (format "%s ok" hello)))))
 
 (ert-deftest irony-iotask-schedule/task-update/simple ()
   (let ((task (irony-iotask-package-task irony-iotask/task-update-t)))
-    (irony-iotask/with-echo-process-setup
+    (irony-iotask/with-echo-process
      (should (string= "hello ok" (irony-iotask-run process task))))))
 
 (ert-deftest irony-iotask-schedule/task-update/with-arguments ()
   (let ((task (irony-iotask-package-task irony-iotask/task-update-t "bonjour")))
-    (irony-iotask/with-echo-process-setup
+    (irony-iotask/with-echo-process
      (should (string= "bonjour ok" (irony-iotask-run process task))))))
 
+(irony-iotask-define-task irony-iotask/task-invalid-msg-t
+  "doc"
+  :start (lambda ()
+           (irony-iotask-send-string "ping\n"))
+  :update (lambda ()
+            (throw 'invalid-msg t)))
+
 (ert-deftest irony-iotask-schedule/task-update/invalid-msg ()
-  (let ((task (irony-iotask-package-task irony-iotask/task-update-t)))
-    (irony-iotask/with-elisp-process-setup
-     (progn
-       (read-from-minibuffer "")
-       (message "spurious-output"))
+  (let ((task (irony-iotask-package-task irony-iotask/task-invalid-msg-t)))
+    (irony-iotask/with-echo-process
      (should-error (irony-iotask-run process task)
                    :type 'irony-iotask-bad-data))))
 
@@ -161,10 +167,7 @@ available on all OSes irony-iotask support."
   (let ((task (irony-iotask-chain
                (irony-iotask-package-task irony-iotask/task-update-t "hi")
                (irony-iotask-package-task irony-iotask/task-update-t "hej"))))
-    (irony-iotask/with-elisp-process-setup
-     (progn
-       (message (read-from-minibuffer ""))
-       (message (read-from-minibuffer "")))
+    (irony-iotask/with-echo-process
      (should (equal "hej ok" (irony-iotask-run process task))))))
 
 (defvar irony-iotask/task-finish-var nil)
@@ -189,14 +192,14 @@ available on all OSes irony-iotask support."
 
 (ert-deftest irony-iotask-schedule/task-finish/simple ()
   (let ((task (irony-iotask-package-task irony-iotask/task-finish-t)))
-    (irony-iotask/with-echo-process-setup
+    (irony-iotask/with-echo-process
      (setq irony-iotask/task-finish-var nil)
      (irony-iotask-run process task)
      (should (equal "how are you?" irony-iotask/task-finish-var)))))
 
 (ert-deftest irony-iotask-schedule/task-on-success/simple ()
   (let ((task (irony-iotask-package-task irony-iotask/task-finish-t)))
-    (irony-iotask/with-echo-process-setup
+    (irony-iotask/with-echo-process
      (setq irony-iotask/task-on-var nil)
      (irony-iotask-run process task)
      (should (equal "success" irony-iotask/task-on-var)))))
@@ -210,8 +213,7 @@ available on all OSes irony-iotask support."
 
 (ert-deftest irony-iotask-schedule/task-on-error/simple ()
   (let ((task (irony-iotask-package-task irony-iotask/task-on-error-t)))
-    (irony-iotask/with-elisp-process-setup
-     () ;; no-op
+    (irony-iotask/with-echo-process
      (setq irony-iotask/task-on-var nil)
      (ignore-errors
        (irony-iotask-run process task))
