c4318ae8655c410980fe98de7fdd93e1af861437
Guillaume Papin <guillaume.papin@epitech.eu>
2018-10-13 16:52:40 +0200

    FindLibClang.cmake: add support for 8.0.0
    This is the version used by trunk.

--- server/cmake/modules/FindLibClang.cmake
+++ server/cmake/modules/FindLibClang.cmake
@@ -15,8 +15,9 @@
 
 # most recent versions come first
 # http://llvm.org/apt/
-set(LIBCLANG_KNOWN_LLVM_VERSIONS 7.0.0 7.0 7
-  6.0.0 6.0.1 6.0 6
+set(LIBCLANG_KNOWN_LLVM_VERSIONS 8.0.0 8.0 8
+  7.0.0 7.0 7
+  6.0.1 6.0.0 6.0 6
   5.0.2 5.0.1 5.0.0 5.0 5
   4.0.1 4.0.0 4.0 4
   3.9.1 3.9.0 3.9
